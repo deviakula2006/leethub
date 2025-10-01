22f1b299fc3e1ea05e022e6db2a07341fe28d0e2
deviakula2006 <147473072+deviakula2006@users.noreply.github.com>
2025-10-01 17:29:04 +0530

    Time: 0 ms (100%), Space: 40.5 MB (43.3%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,5 +1,18 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        
-    }
+        int sum =numBottles;
+        int num = numExchange;
+        while(numBottles/num>0){
+          int div = numBottles/num;
+          if(numBottles%num ==0){
+         numBottles = numBottles/num;
+          }
+          else {
+            numBottles = numBottles/num + numBottles%num;
+          }
+          sum+=div;
+          
+        }
+        return sum;
+}
 }
