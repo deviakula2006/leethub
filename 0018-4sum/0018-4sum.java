55fa355786c446c3470778c41eb1f3e8ffaa3e94
AKULA DEVI GANGA BHAVANI <23A91A0501@aec.edu.in>
2026-04-12 20:49:48 +0530

    Time: 298 ms (5.19%), Space: 47.4 MB (9.13%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+       
+
+        Arrays.sort(nums);
+ Set <List<Integer>> set = new HashSet<>();
+        for(int i=0;i<nums.length-3;i++){
+            for(int j=i+1;j<nums.length-2;j++){
+            int l = j+1;
+            int r = nums.length-1;
+            while(l<r){
+                long x =(long)nums[l]+nums[r]+nums[i]+nums[j];
+                if(x==target){
+               set.add(Arrays.asList(nums[i],nums[l],nums[r],nums[j]));
+               l++;
+               r=nums.length-1;
+                }
+                else if(x<target){
+                    l++;
+                }
+                else {
+                    r--;
+                }
+
+                }
+            }
+        }
+             List<List<Integer>> list = new ArrayList<>(set);
+        return list;
+        }
+       
+    }
+
+    
