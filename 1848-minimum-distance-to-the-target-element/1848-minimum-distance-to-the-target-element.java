667f9da15878ad70d0b86e0d2c8c4e9fd520780c
AKULA DEVI GANGA BHAVANI <23A91A0501@aec.edu.in>
2026-04-13 21:02:51 +0530

    Time: 1 ms (47.94%), Space: 44.8 MB (69.96%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n = nums.length;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+if(nums[i]==target){
+    int a = Math.abs(i-start);
+    min =Math.min(min,a);
+}
+        }
+        return min;
+    }
+}
