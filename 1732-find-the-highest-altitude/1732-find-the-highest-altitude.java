911543e7f98e6bcd47344a6aef9c79416fcd7e76
AKULA DEVI GANGA BHAVANI <23A91A0501@aec.edu.in>
2026-06-19 22:01:47 +0530

    Time: 0 ms (100%), Space: 43.2 MB (18.48%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int prev=0;
+        for(int i=0;i<gain.length;i++){
+      max=Math.max(prev,max);
+      prev+=gain[i];
+        }
+        max=Math.max(prev,max);
+        return max;
+    }
+}
